8e45773b225de620854d0dde5b33ad765651d002
amoravski <alexmoravski@gmail.com>
2020-02-27 18:59:04 +0200

    Final version

--- amoravski-villages/get_villages.py
+++ amoravski-villages/get_villages.py
@@ -1,8 +1,24 @@
+#!/usr/bin/env python3
 import cgi
+import json
 import postgre_villages
 
-postgre_villages.get_villages(filter)
+fs = cgi.FieldStorage()
+if 'filter' in fs:
+    result = postgre_villages.get_villages(fs['filter'].value + '%' )
+else:
+    result = postgre_villages.get_villages("%")
 
-print("Content-Type: text/plain;charset=utf-8")
+final_dict = dict()
+for index, result_str in enumerate(result):
+    temp_dict = dict()
+    result_str = result_str[1:-1].split(',')
+    temp_dict['name'] = postgre_villages.sanitize_string(result_str[0])
+    temp_dict['obstina'] = postgre_villages.sanitize_string(result_str[1])
+    temp_dict['oblast'] = postgre_villages.sanitize_string(result_str[2])
+    final_dict['{}'.format(index)] = temp_dict
+
+print("Content-Type: application/json;charset=utf-8")
+print("Access-Control-Allow-Origin: *")
 print()
-print("Hello World!")
+print(json.dumps(final_dict))
--- amoravski-villages/postgre_villages.py
+++ amoravski-villages/postgre_villages.py
@@ -1,8 +1,18 @@
+import re
 import psycopg2
 import dump_data
 
+
 def execute_query(query,result):
+    ''' Connect to postgre and execute query
+        Params:
+        - str query
+        - bool result - Query gets result if true
+        Returns:
+        - str result_str
+    '''
     result_str =''
+    #print(query)
     try:
         connection = psycopg2.connect(user = "amoravski",
                                      password = "strongpassword",
@@ -24,12 +34,21 @@ def execute_query(query,result):
             return result_str
         return
 
+
+def sanitize_string(string):
+    return re.sub(r"[^a-zA-Z0-9а-яА-Я ]", "", string)
+
+
 def master_insert():
+    ''' Constructs all queries to run, done at the same time
+        because of spreadsheet format
+    '''
     vil_insert = "INSERT INTO villages(ekatte, name, obstina) VALUES"
     mun_insert = "INSERT INTO municipality(obstina, name, oblast) VALUES"
     reg_insert = "INSERT INTO region(oblast,name) VALUES"
 
     vil_data = dump_data.get_values_from_spreadsheet("Ek_atte.xlsx")
+    # First row is irrelevant on Ek_atte
     del vil_data[0]
     mun_data = dump_data.get_values_from_spreadsheet("Ek_obst.xlsx")
     reg_data = dump_data.get_values_from_spreadsheet("Ek_obl.xlsx")
@@ -41,12 +60,12 @@ def master_insert():
         if vil_row['ekatte'] in mun_ekkates:
             muns = [ mun for mun in mun_data if vil_row['ekatte']==mun['ekatte']]
             for mun in muns:
-                mun_insert += " (\'{}\',\'{}\',\'{}\'),".format(mun["obstina"],mun["name"],vil_row["oblast"])
+                mun_insert += " (\'{}\',\'{}\',\'{}\'),".format(sanitize_string(mun["obstina"]),sanitize_string(mun["name"]),sanitize_string(vil_row["oblast"]))
         if vil_row['ekatte'] in reg_ekkates:
             regs = [ reg for reg in reg_data if vil_row['ekatte']==reg['ekatte']]
             for reg in regs:
-                reg_insert += " (\'{}\',\'{}\'),".format(reg["oblast"],reg["name"])
-        vil_insert += " (\'{}\',\'{}\',\'{}\'),".format(vil_row["ekatte"],vil_row["name"],vil_row["obstina"])
+                reg_insert += " (\'{}\',\'{}\'),".format(sanitize_string(reg["oblast"]),sanitize_string(reg["name"]))
+        vil_insert += " (\'{}\',\'{}\',\'{}\'),".format(sanitize_string(vil_row["ekatte"]),sanitize_string(vil_row["name"]),sanitize_string(vil_row["obstina"]))
 
     vil_insert = vil_insert[:-1]
     vil_insert += " ON CONFLICT (ekatte) DO UPDATE SET name = excluded.name, obstina = excluded.obstina;"
@@ -61,18 +80,32 @@ def master_insert():
     execute_query(mun_insert, False)
     execute_query(vil_insert, False)
 
+
 def get_statistics(tables):
+    ''' Gets number of rows in supplied tables
+        Params:
+        - list tables
+        Return:
+        - list of tuples ( name, rows )
+    '''
     for table in tables:
         yield (table, execute_query("SELECT COUNT(*) FROM {};".format(table), True)[0][0])
 
 def get_villages(filter):
-    result = execute_query("SELECT (name) FROM villages WHERE name LIKE \'{}%\';".format(filter), True)
+    ''' Gets villages, can be filtered
+        Params:
+        - str filter - case insensitive village name filter
+        Return:
+        - list of tuples (village name, municipality name, region name)
+    '''
+    result = execute_query("SELECT (villages.name, municipality.name, region.name) FROM villages INNER JOIN municipality ON(villages.obstina = municipality.obstina) INNER JOIN region ON (municipality.oblast = region.oblast) WHERE villages.name ILIKE \'{}%\';".format(sanitize_string(filter)), True)
     return [ x[0] for x in result ]
 
 if __name__ == "__main__":
+    # Demonstration if ran as main
     print("Filling up tables...")
     master_insert()
     print("Getting row number for tables...")
     for value in get_statistics(["villages","municipality","region"]):
         print(value)
-    print(get_villages("Бан"))
+    print(get_villages("бяла"))
