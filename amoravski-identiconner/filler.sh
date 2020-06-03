378f7bdae872b52c761a48195f244aad27c9f32e
amoravski <alexmoravski@gmail.com>
2020-06-03 17:59:59 +0300

    Changed identiconner filler

--- amoravski-identiconner/filler.sh
+++ amoravski-identiconner/filler.sh
@@ -4,6 +4,6 @@ counter=1
 limit=10000
 while [ $counter -le $limit ]
 do
-    curl -X POST "http://localhost:3000/product" -F "name=\"${counter}\"" -F "quantity=$counter" -F "added_by=1" -F "price=$counter" -F "created_at=1583160772" -F 'tags=["Ninja","Sinzddza","Gdadwzaa"]' -F "file=@$counter.png"
+    curl -X POST "http://localhost:3000/product" -F "name=\"${counter}\"" -F "quantity=$counter" -F "added_by=1" -F "price=$(($counter*100))" -F "created_at=1583160772" -F 'tags=["Ninja","Sinzddza","Gdadwzaa"]' -F "file=@$counter.png"
     ((counter ++))
 done
