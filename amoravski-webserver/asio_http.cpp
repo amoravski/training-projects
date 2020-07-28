5cb55df2eaedf997ea13763120e1010d6546fb91
amoravski <alexmoravski@gmail.com>
2020-07-28 18:17:49 +0300

    Added static file serving

--- amoravski-webserver/asio_http.cpp
+++ amoravski-webserver/asio_http.cpp
@@ -15,296 +15,297 @@ class session;
 
 class http_headers
 {
-   std::string method;
-   std::string url;
-   std::string version;
+    std::string method;
+    std::string url;
+    std::string version;
 
-   std::map<std::string, std::string> params;
-   
-   std::map<std::string, std::string> headers;
+    std::map<std::string, std::string> params;
+
+    std::map<std::string, std::string> headers;
 
 public:
-   
-   std::string get_response()
-   {
-      std::stringstream ssOut;
-      std::cout << url <<std::endl;
-      try {
-      if(url == "/" || url == "/index.html" )
-      {
-         ssOut << "HTTP/1.1 200 OK" << std::endl;
-         ssOut << "content-type: text/html" << std::endl;
 
-         std::string line;
-         std::ifstream html_file ("index.html");
-         fs::path p = fs::current_path() / "index.html";
-         ssOut << "content-length: " << fs::file_size(p) << std::endl;
-         ssOut << std::endl;
+    std::string get_response()
+    {
+        std::stringstream ssOut;
+        try {
+            std::string format = url.substr(url.find_last_of(".")+1);
+            if(url == "/" || url == "/index.html" )
+            {
+                ssOut << "HTTP/1.1 200 OK" << std::endl;
+                ssOut << "content-type: text/html; charset=utf-8" << std::endl;
 
-         if(html_file.is_open()) 
-         {
-             while(getline(html_file,line))
-             {
-                 ssOut << line << '\n';
-             }
-             html_file.close();
-         }
-      }
+                std::string line;
+                std::ifstream html_file ("index.html");
+                fs::path p = fs::current_path() / "index.html";
+                ssOut << "content-length: " << fs::file_size(p) << std::endl;
+                ssOut << std::endl;
 
-      else if(url!= "" && fs::exists(url.substr(1, std::string::npos)))
-      {
-         ssOut << "HTTP/1.1 200 OK" << std::endl;
-         ssOut << "content-type: text/html" << std::endl;
+                if(html_file.is_open())
+                {
+                    while(getline(html_file,line))
+                    {
+                        ssOut << line << '\n';
+                    }
+                    html_file.close();
+                }
+            }
 
-         std::string line;
-         std::ifstream html_file (url.substr(1, std::string::npos));
-         fs::path p = fs::current_path() / url.substr(1, std::string::npos);
-         ssOut << "content-length: " << fs::file_size(p) << std::endl;
-         ssOut << std::endl;
+            else if(url!= "" && fs::exists(url.substr(1, std::string::npos)) && headers.find("Accept")->second.compare(std::string{"image/webp"}) && (format == "ico" || format == "png" || format == "jpg")) {
+                ssOut << "HTTP/1.1 200 OK" << std::endl;
+                ssOut << "content-type: image/webp" << std::endl;
 
-         if(html_file.is_open()) 
-         {
-             while(getline(html_file,line))
-             {
-                 ssOut << line << '\n';
-             }
-             html_file.close();
-         }
-      }
+                std::string line;
+                std::ifstream html_file (url.substr(1, std::string::npos));
+                fs::path p = fs::current_path() / url.substr(1, std::string::npos);
+                ssOut << "content-length: " << fs::file_size(p) << std::endl;
+                ssOut << std::endl;
 
-      /*
-      else if(Is file?) {
-         ssOut << "HTTP/1.1 200 OK" << std::endl;
-         ssOut << "content-type: text/html" << std::endl;
+                if(html_file.is_open())
+                {
+                    while(getline(html_file,line))
+                    {
+                        ssOut << line << '\n';
+                    }
+                    html_file.close();
+                }
+            }
 
-         std::string line;
-         std::ifstream html_file (url.substr(1, std::string::npos));
-         fs::path p = fs::current_path() / url.substr(1, std::string::npos);
-         ssOut << "content-length: " << fs::file_size(p) << std::endl;
-         ssOut << std::endl;
+            else if(url!= "" && fs::exists(url.substr(1, std::string::npos)))
+            {
+                ssOut << "HTTP/1.1 200 OK" << std::endl;
+                ssOut << "content-type: text/html; charset=utf-8" << std::endl;
 
-         if(html_file.is_open()) 
-         {
-             while(getline(html_file,line))
-             {
-                 ssOut << line << '\n';
-             }
-             html_file.close();
-         }
-      }
-        */
-      else
-      {
-         std::string line;
-         std::ifstream html_file ("404.html");
-         fs::path p = fs::current_path() / "404.html";
-         std::string sHTML = "<html><body><h1>404 Not Found</h1><p>There's nothing here.</p></body></html>";
-         ssOut << "HTTP/1.1 404 Not Found" << std::endl;
-         ssOut << "content-type: text/html" << std::endl;
-         ssOut << "content-length: " << fs::file_size(p) << std::endl;
-         ssOut << std::endl;
-        if(html_file.is_open()) 
-         {
-             while(getline(html_file,line))
-             {
-                 ssOut << line << '\n';
-             }
-             html_file.close();
-         }
-      }
-      return ssOut.str();
-      }
-      catch (...) {
-         return on_internal_error();
-      }
-   }
-   
+                std::string line;
+                std::ifstream html_file (url.substr(1, std::string::npos));
+                fs::path p = fs::current_path() / url.substr(1, std::string::npos);
+                ssOut << "content-length: " << fs::file_size(p) << std::endl;
+                ssOut << std::endl;
+
+                if(html_file.is_open())
+                {
+                    while(getline(html_file,line))
+                    {
+                        ssOut << line << '\n';
+                    }
+                    html_file.close();
+                }
+            }
+
+            else
+            {
+                std::string line;
+                std::ifstream html_file ("404.html");
+                fs::path p = fs::current_path() / "404.html";
+                std::string sHTML = "<html><body><h1>404 Not Found</h1><p>There's nothing here.</p></body></html>";
+                ssOut << "HTTP/1.1 404 Not Found" << std::endl;
+                ssOut << "content-type: text/html" << std::endl;
+                ssOut << "content-length: " << fs::file_size(p) << std::endl;
+                ssOut << std::endl;
+                if(html_file.is_open())
+                {
+                    while(getline(html_file,line))
+                    {
+                        ssOut << line << '\n';
+                    }
+                    html_file.close();
+                }
+            }
+
+            return ssOut.str();
+        }
+        catch (...) {
+            return on_internal_error();
+        }
+    }
+
     std::string on_internal_error() {
-         std::string line;
-         std::ifstream html_file ("500.html");
-         fs::path p = fs::current_path() / "500.html";
+        std::string line;
+        std::ifstream html_file ("500.html");
+        fs::path p = fs::current_path() / "500.html";
         std::stringstream ssOut;
         ssOut << "HTTP/1.1 500 Internal Error" << std::endl;
         ssOut << "content-type: text/html" << std::endl;
-         ssOut << "content-length: " << fs::file_size(p) << std::endl;
+        ssOut << "content-length: " << fs::file_size(p) << std::endl;
         ssOut << std::endl;
-        if(html_file.is_open()) 
-         {
-             while(getline(html_file,line))
-             {
-                 ssOut << line << '\n';
-             }
-             html_file.close();
-         }
+        if(html_file.is_open())
+        {
+            while(getline(html_file,line))
+            {
+                ssOut << line << '\n';
+            }
+            html_file.close();
+        }
         return ssOut.str();
-   }
-   
-   int content_length()
-   {
-      auto request = headers.find("content-length");
-      if(request != headers.end())
-      {
-         std::stringstream ssLength(request->second);
-         int content_length;
-         ssLength >> content_length;
-         return content_length;
-      }
-      return 0;
-   }
-   
-   void on_read_header(std::string line)
-   {
-      std::cout << "header: " << line << std::endl;
-      
-      std::stringstream ssHeader(line);
-      std::string headerName;
-      std::getline(ssHeader, headerName, ':');
-      
-      std::string value;
-      std::getline(ssHeader, value);
-      headers[headerName] = value;
-   }
-   
-   void on_read_request_line(std::string line)
-   {
-      std::stringstream ssRequestLine(line);
+    }
 
-      ssRequestLine >> method;
-      ssRequestLine >> url;
-      ssRequestLine >> version;
+    int content_length()
+    {
+        auto request = headers.find("content-length");
+        if(request != headers.end())
+        {
+            std::stringstream ssLength(request->second);
+            int content_length;
+            ssLength >> content_length;
+            return content_length;
+        }
+        return 0;
+    }
 
-      std::cout << "method: " << method << " ";
-      std::cout << "url: " << url << " ";
-      std::cout << "version: " << version << std::endl;
-      std::vector<std::string> temp;
-      try {
-          boost::split(temp,url,boost::is_any_of("?&="));
-          url=temp[0];
-          temp.erase(temp.begin());
-          for(auto it = temp.begin(); it!=temp.end(); it+=2) {
-              params.insert(std::pair<std::string,std::string>(*it, *(it+1)));
-          }
-          for (std::map<std::string, std::string>::const_iterator it = params.begin(); it != params.end(); ++it) {
-              std::cout << it->first << " " << it->second << " ";
-          }
-      }
-      catch (...){
+    void on_read_header(std::string line)
+    {
+        std::cout << "header: " << line << std::endl;
+
+        std::stringstream ssHeader(line);
+        std::string headerName;
+        std::getline(ssHeader, headerName, ':');
+
+        std::string value;
+        std::getline(ssHeader, value);
+        headers[headerName] = value;
+    }
+
+    void on_read_request_line(std::string line)
+    {
+        std::stringstream ssRequestLine(line);
+
+        ssRequestLine >> method;
+        ssRequestLine >> url;
+        ssRequestLine >> version;
+
+        std::cout << "method: " << method << " ";
+        std::cout << "url: " << url << " ";
+        std::cout << "version: " << version << std::endl;
+        std::vector<std::string> temp;
+        try {
+            boost::split(temp,url,boost::is_any_of("?&="));
+            url=temp[0];
+            temp.erase(temp.begin());
+            for(auto it = temp.begin(); it!=temp.end(); it+=2) {
+                params.insert(std::pair<std::string,std::string>(*it, *(it+1)));
+            }
+            for (std::map<std::string, std::string>::const_iterator it = params.begin(); it != params.end(); ++it) {
+                std::cout << it->first << " " << it->second << " ";
+            }
+        }
+        catch (...){
             std::cout << "Incorrectly formatted params, disregarding..." << std::endl;
-      }
+        }
 
-      std::cout << "request for resource: " << url << std::endl;
-   }
+        std::cout << "request for resource: " << url << std::endl;
+    }
 };
 
 class session
 {
-   asio::streambuf buff;
-   http_headers headers;
-   
-   static void read_body(std::shared_ptr<session> pThis)
-   {
-      int nbuffer = 1000;
-      std::shared_ptr<std::vector<char>> bufptr = std::make_shared<std::vector<char>>(nbuffer);
-      asio::async_read(pThis->socket, boost::asio::buffer(*bufptr, nbuffer), [pThis](const error_code& e, std::size_t s)
-      {
-        //Handle stuff in body
-      });
-   }
-   
-   static void read_next_line(std::shared_ptr<session> pThis)
-   {
-      asio::async_read_until(pThis->socket, pThis->buff, '\r', [pThis](const error_code& e, std::size_t s)
-      {
-         std::string line, ignore;
-         std::istream stream {&pThis->buff};
-         std::getline(stream, line, '\r');
-         std::getline(stream, ignore, '\n');
+    asio::streambuf buff;
+    http_headers headers;
 
-         pThis->headers.on_read_header(line);
-         
-         if(line.length() == 0)
-         {
-            if(pThis->headers.content_length() == 0)
+    static void read_body(std::shared_ptr<session> pThis)
+    {
+        int nbuffer = 1000;
+        std::shared_ptr<std::vector<char>> bufptr = std::make_shared<std::vector<char>>(nbuffer);
+        asio::async_read(pThis->socket, boost::asio::buffer(*bufptr, nbuffer), [pThis](const error_code& e, std::size_t s)
+        {
+            //Handle stuff in body somehow
+        });
+    }
+
+    static void read_next_line(std::shared_ptr<session> pThis)
+    {
+        asio::async_read_until(pThis->socket, pThis->buff, '\r', [pThis](const error_code& e, std::size_t s)
+        {
+            std::string line, ignore;
+            std::istream stream {&pThis->buff};
+            std::getline(stream, line, '\r');
+            std::getline(stream, ignore, '\n');
+
+            pThis->headers.on_read_header(line);
+
+            if(line.length() == 0)
             {
-               std::shared_ptr<std::string> str = std::make_shared<std::string>(pThis->headers.get_response());
-               asio::async_write(pThis->socket, boost::asio::buffer(str->c_str(), str->length()), [pThis, str](const error_code& e, std::size_t s)
-               {
-                  std::cout << "done" << std::endl;
-               });
+                if(pThis->headers.content_length() == 0)
+                {
+                    std::shared_ptr<std::string> str = std::make_shared<std::string>(pThis->headers.get_response());
+                    asio::async_write(pThis->socket, boost::asio::buffer(str->c_str(), str->length()), [pThis, str](const error_code& e, std::size_t s)
+                    {
+                        std::cout << "done" << std::endl;
+                    });
+                }
+                else
+                {
+                    pThis->read_body(pThis);
+                }
             }
             else
             {
-               pThis->read_body(pThis);
+                pThis->read_next_line(pThis);
             }
-         }
-         else
-         {
-            pThis->read_next_line(pThis);
-         }
-      });
-   }
-   
-   static void read_first_line(std::shared_ptr<session> pThis)
-   {
-      asio::async_read_until(pThis->socket, pThis->buff, '\r', [pThis](const error_code& e, std::size_t s)
-      {
-         std::string line, ignore;
-         std::istream stream {&pThis->buff};
-         std::getline(stream, line, '\r');
-         std::getline(stream, ignore, '\n');
-         pThis->headers.on_read_request_line(line);
-         pThis->read_next_line(pThis);
-      });
-   }
+        });
+    }
 
-   static void send_internal_error(std::shared_ptr<session> pThis)
-   {
-               std::shared_ptr<std::string> str = std::make_shared<std::string>(pThis->headers.on_internal_error());
-               asio::async_write(pThis->socket, boost::asio::buffer(str->c_str(), str->length()), [pThis, str](const error_code& e, std::size_t s)
-               {
-                  std::cout << "done" << std::endl;
-               });
-   }
-   
+    static void read_first_line(std::shared_ptr<session> pThis)
+    {
+        asio::async_read_until(pThis->socket, pThis->buff, '\r', [pThis](const error_code& e, std::size_t s)
+        {
+            std::string line, ignore;
+            std::istream stream {&pThis->buff};
+            std::getline(stream, line, '\r');
+            std::getline(stream, ignore, '\n');
+            pThis->headers.on_read_request_line(line);
+            pThis->read_next_line(pThis);
+        });
+    }
+
+    static void send_internal_error(std::shared_ptr<session> pThis)
+    {
+        std::shared_ptr<std::string> str = std::make_shared<std::string>(pThis->headers.on_internal_error());
+        asio::async_write(pThis->socket, boost::asio::buffer(str->c_str(), str->length()), [pThis, str](const error_code& e, std::size_t s)
+        {
+            std::cout << "done" << std::endl;
+        });
+    }
+
 public:
 
-   ip::tcp::socket socket;
-   
-   session(io_service& io_service)
-      :socket(io_service)
-   {
-   }
-   
-   static void interact(std::shared_ptr<session> pThis) try
-   {
-            read_first_line(pThis);
-   } catch (const std::exception& e) {
-            send_internal_error(pThis);
-   }
+    ip::tcp::socket socket;
+
+    session(io_service& io_service)
+    :socket(io_service)
+    {
+    }
+
+    static void interact(std::shared_ptr<session> pThis) try
+    {
+        read_first_line(pThis);
+    } catch (const std::exception& e) {
+        send_internal_error(pThis);
+    }
 };
 
 void accept_and_run(ip::tcp::acceptor& acceptor, io_service& io_service)
 {
-   std::shared_ptr<session> sesh = std::make_shared<session>(io_service);
-   acceptor.async_accept(sesh->socket, [sesh, &acceptor, &io_service](const error_code& accept_error)
-   {
-      accept_and_run(acceptor, io_service);
-      if(!accept_error)
-      {
-         session::interact(sesh);
-      }
-   });
+    std::shared_ptr<session> sesh = std::make_shared<session>(io_service);
+    acceptor.async_accept(sesh->socket, [sesh, &acceptor, &io_service](const error_code& accept_error)
+    {
+        accept_and_run(acceptor, io_service);
+        if(!accept_error)
+        {
+        session::interact(sesh);
+        }
+    });
 }
 
 int main(int argc, const char * argv[])
 {
-   io_service io_service;
-   ip::tcp::endpoint endpoint{ip::tcp::v4(), 8080};
-   ip::tcp::acceptor acceptor{io_service, endpoint};
-   
-   acceptor.listen();
-   accept_and_run(acceptor, io_service);
-   
-   io_service.run();
-   return 0;
+    io_service io_service;
+    // Initialize endpoint on port 8080
+    ip::tcp::endpoint endpoint{ip::tcp::v4(), 8080};
+    ip::tcp::acceptor acceptor{io_service, endpoint};
+
+    acceptor.listen();
+    accept_and_run(acceptor, io_service);
+
+    io_service.run();
+    return 0;
 }
